1ae4b1ede01c9df4de26645447427063a4bc42c0
Felix Watts <felixwatts@gmail.com>
2024-07-12 21:30:10 +0100

    typo

--- server.go
+++ server.go
@@ -20,7 +20,7 @@ import (
 )
 
 const portHttp = 8200
-const fileSystemRoot = "/home/felix/code/chezwatts.gallery/"
+const fileSystemRoot = "/home/ubuntu/data/chezwatts.gallery/"
 const contentRoot = fileSystemRoot + "content/"
 const galleriesRoot = contentRoot + "galleries/"
 const statsLogFilename = "stats_log.csv"
